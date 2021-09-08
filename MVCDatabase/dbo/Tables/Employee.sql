ceb161864a8643928c47938407d655a9cc3be132
Albert Attila <albattis@freemail.hu>
2021-09-08 11:07:41 +0200

    add project for github

---
+++ MVCDatabase/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [EmployeeId] INT NOT NULL, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmailAddress] VARCHAR(50) NOT NULL
+)
---
+++ MVCWeabApplication/Models/EmployeeModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCWeabApplication.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name ="Employee Id")]
+        [Range(100000,999999,ErrorMessage ="You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="You need to give us your First Name. ")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to give us your Last Name. ")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to give us your email address. ")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name ="Confirm Email")]
+        [Compare("EmailAddress",ErrorMessage ="The email and confrim email must match")]
+        public string ConfirmEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(100,MinimumLength =10,ErrorMessage ="You need to provide a long enought password.")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="You password and Confirm password do not match")]
+        public string ConfrimPassword { get; set; }
+    }
+}
