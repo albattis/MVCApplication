c1fa38ab598b178a7ef9e842abb6d5183882c8a3
Albert Attila <albattis@freemail.hu>
2021-09-08 12:42:41 +0200

    controller

--- DataLibrary/BussinessLogic/EmployeeProcessor.cs
+++ DataLibrary/BussinessLogic/EmployeeProcessor.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLibrary.DataAccess;
+using DataLibrary.Models;
 
 namespace DataLibrary.BussinessLogic
 {
-    class EmployeeProcessor
+    public static class EmployeeProcessor
     {
+        public static int CreateEmployee(int employeeId, string firstname, string lastname, string emailaddress)
+        {
+            EmployeeModel data = new EmployeeModel()
+            {
+                EmployeeId=employeeId,
+                FirstName=firstname,
+                LastName=lastname,
+                EmailAddress=emailaddress
+            };
+            string sql = @"insert into dbo.Employee(EmployeeId,FirstName,LastName,EmailAddress)
+                            values(@EmployeeId,@FirstName,@LastName,@EmailAddress); ";
+            return SQLDataAccess.SaveData(sql, data);
+        }
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = "select Id,EmployeeId,FirstName,Lastname,EmailAddress from dbo.Employee;";
+
+            return SQLDataAccess.LoadData<EmployeeModel>(sql);
+        }
     }
 }
--- MVCWeabApplication/Controllers/HomeController.cs
+++ MVCWeabApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static DataLibrary.BussinessLogic.EmployeeProcessor;
 using MVCWeabApplication.Models;
 
 namespace MVCWeabApplication.Controllers
@@ -39,6 +40,7 @@ namespace MVCWeabApplication.Controllers
         {
             if (ModelState.IsValid)
             {
+                int recordsCreated=CreateEmployee(model.EmployeeId,model.FirstName,model.LastName,model.EmailAddress);
                 return RedirectToAction("Index");
             }
 
