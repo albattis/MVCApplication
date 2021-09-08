dd81bcf2feebc60af00ec968c3d1e38520a75db1
Albert Attila <albattis@freemail.hu>
2021-09-08 11:27:58 +0200

    add diaper and sql

---
+++ DataLibrary/DataAccess/SQLDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DataLibrary.DataAccess
+{
+    public static class SQLDataAccess
+    {
+        public static string GetConnenctionString(string ConnectionName="MVCDatabase")
+        {
+            return ConfigurationManager.ConnectionStrings[ConnectionName].ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection connenction=new SqlConnection(GetConnenctionString()))
+            {
+                return connenction.Query<T>(sql).ToList();
+            }
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection connenction = new SqlConnection(GetConnenctionString()))
+            {
+                return connenction.Execute(sql, data);
+            }
+        }
+
+    }
+}
